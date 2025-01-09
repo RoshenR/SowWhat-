fb89f299668bad4c89e490d0f639bbb1d343bf87
Saikorenji <eliec337@gmail.com>
2025-01-09 09:28:11 +0100

    Mise a jour du routes.php

--- config/routes.php
+++ config/routes.php
@@ -8,6 +8,34 @@ const AVAILABLE_ROUTES = [
             'description' => 'Accueil tah les bogota..',
         ]
     ],
+    'contact' => [
+		'controller' => 'contactController.php',
+		'seo' => [
+			'title' => 'Contact',
+			'description' => 'Ma meta desc...',
+		]
+		],
+    'legal_mentions' => [
+		'controller' => 'legalMentionsController.php',
+		'seo' => [
+			'title' => 'Mention légales',
+			'description' => 'Ma meta desc...',
+		]
+		],
+    'team' => [
+        'controller' => 'teamController.php',
+        'seo' => [
+            'title' => 'Team',
+            'description' => 'Ma meta desc...',
+        ]
+        ],
+    'privacy_policy' => [
+		'controller' => 'privacyPolicyController.php',
+		'seo' => [
+			'title' => 'Politique de confidentialité',
+			'description' => 'Ma meta desc...',
+		]
+        ],
 ];
 
 const DEFAULT_ROUTE = AVAILABLE_ROUTES['home'];
