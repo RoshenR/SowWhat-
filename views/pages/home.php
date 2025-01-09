24846759192010b8b0d0ca97de2f9d6f1ce6a9ef
RoshenR <crisliath@gmail.com>
2025-01-09 13:01:56 +0100

    REMOVE/-Navbar ADD/-Menu-Burger

--- views/pages/home.php
+++ views/pages/home.php
@@ -10,23 +10,35 @@
         <div class="info-card">
             <h3>Dénoncer une balise</h3>
             <p>Signalez rapidement toute balise défectueuse en un clic.</p>
+            <button><span>Dénoncer ici</span> <ion-icon name="arrow-forward-outline"></ion-icon></button>
         </div>
         <div class="info-card">
             <h3>Stats</h3>
             <p>Accédez à des statistiques détaillées sur les balises signalées.</p>
+            <button><span>Voir les stats</span> <ion-icon name="arrow-forward-outline"></ion-icon></button>
         </div>
         <div class="info-card">
             <h3>Récap</h3>
             <p>Consultez un résumé des balises signalées récemment.</p>
+            <button><span>Consulter ici</span> <ion-icon name="arrow-forward-outline"></ion-icon></button>
         </div>
     </div>
 
+    <div class="separator"></div>
+
     <div class="a-little-more-main">
-        <p>Nous sommes une jeune entreprise dynamique créée par 4 étudiants passionnés et engagés.
-            Notre objectif est simple : résoudre le problème des balises "SoWeSign" défectueuses
-            pour offrir une expérience optimale aux élèves. Nous croyons en l'importance d'une
-            technologie fiable et fonctionnelle, et nous mettons tout en œuvre pour assurer un service
-            efficace et innovant. Ensemble, nous œuvrons pour simplifier la gestion et l'entretien de ces balises,
-            en alliant expertise technique et esprit d'équipe.</p>
+
+        <h2>Qui sommes-nous?</h2>
+
+        <p>
+            Nous sommes une jeune entreprise dynamique créée par <strong>4 étudiants passionnés et engagés</strong>.
+            Notre objectif est simple : <strong>résoudre le problème des balises "SoWeSign" défectueuses</strong>
+            pour offrir une expérience optimale aux élèves.
+            Nous croyons en l'importance d'une <strong>technologie fiable et fonctionnelle</strong>, et nous mettons tout en œuvre
+            pour assurer un service <em>efficace</em> et <em>innovant</em>.
+            Ensemble, nous œuvrons pour <strong>simplifier la gestion et l'entretien</strong> de ces balises,
+            en alliant <em>expertise technique</em> et <em>esprit d'équipe</em>.
+        </p>
     </div>
+
 </div>
