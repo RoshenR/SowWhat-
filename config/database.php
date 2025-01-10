7224a81dfde3fc1087f163b214c702a02cd253c9
Saikorenji <eliec337@gmail.com>
2025-01-10 10:31:52 +0100

    merge master

--- config/database.php
+++ config/database.php
@@ -7,3 +7,13 @@ const DB_CONFIG = [
     'username' => 'root',
     'password' => 'Ren55HELL'
 ];
+
+try {
+    $dsn = "mysql:host=" . DB_CONFIG['host'] . ";port=" . DB_CONFIG['port'] . ";dbname=" . DB_CONFIG['dbname'] . ";charset=utf8";
+    $pdo = new PDO($dsn, DB_CONFIG['username'], DB_CONFIG['password']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
+
+?>
--- views/pages/contact.php
+++ views/pages/contact.php
@@ -1,20 +1,22 @@
-<div id="home">
-    <h1>Contact</h1>
-    <body>
-    <form action="contact.php" method="post">
-        <label for="name">Nom :</label>
-        <input type="text" id="name" name="name" required><br><br>
-        
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" required><br><br>
-        
-        <label for="subject">Sujet :</label>
-        <input type="text" id="subject" name="subject"><br><br>
-        
-        <label for="message">Message :</label>
-        <textarea id="message" name="message" rows="5" required></textarea><br><br>
-        
-        <button type="submit" name="submit">Envoyer</button>
+<section class="contact-section">
+    <h1>Contactez-nous</h1>
+    <form action="../controllers/contactController.php" method="post">
+        <div class="form-group">
+            <label for="name">Nom :</label>
+            <input type="text" id="name" name="name" placeholder="Votre nom" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email :</label>
+            <input type="email" id="email" name="email" placeholder="Votre email" required>
+        </div>
+        <div class="form-group">
+            <label for="subject">Sujet :</label>
+            <input type="text" id="subject" name="subject" placeholder="Sujet (optionnel)">
+        </div>
+        <div class="form-group">
+            <label for="message">Message :</label>
+            <textarea id="message" name="message" placeholder="Votre message" required></textarea>
+        </div>
+        <button type="submit">Envoyer</button>
     </form>
-</body>
-</div>
+</section>
